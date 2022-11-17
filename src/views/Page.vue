9bfad40f001599a3b03ab7f1908ff4e1b109afbc
formaxabc <863447136@qq.com>
2022-11-17 18:52:41 +0800

    博客翻页

--- src/views/Page.vue
+++ src/views/Page.vue
@@ -1,16 +1,2 @@
-<script setup lang="ts">
-import Blogs from "@/views/Blogs.vue";
-import Pageswitch from "@/views/Pageswitch.vue";
-import { ref, reactive } from "vue"
-let pagenum = ref(0);
-</script>
 
-<template>
-    <Blogs v-model:modelValue="pagenum"></Blogs>
-    <Pageswitch v-model:modelValue="pagenum"></Pageswitch>
-</template>
-
-<style>
-
-</style>
 
