3cc9831f878ca715e755a466f9e737bed0fbb52d
formaxabc <863447136@qq.com>
2022-11-18 19:47:14 +0800

    换页小bug

--- src/views/Pageswitch.vue
+++ src/views/Pageswitch.vue
@@ -43,6 +43,13 @@ let currentPage = ref(1);
 function changeBtn1(){
     currentPage.value = 1;
     emit('update:modelValue', currentPage.value)
+    pagebtns = reactive([
+    {id:1,num:1},
+    {id:2,num:2},
+    {id:3,num:3},
+    {id:4,num:4},
+    {id:5,num:5}
+    ])
 }
 
 function changeBtnpre(){
