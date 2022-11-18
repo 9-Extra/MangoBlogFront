3571b3c342841aeff31d0042ca10c92a044c4281
9-Extra <77313011@qq.com>
2022-11-18 15:36:29 +0800

    实现editor页面id检查和跳转

--- src/component/Editor.vue
+++ src/component/Editor.vue
@@ -3,6 +3,20 @@ import editor from "mavon-editor"
 import 'mavon-editor/dist/css/index.css'
 import {ref} from "vue"
 
+function get_blog_id(): number | null {
+    let params = new URLSearchParams(document.location.search.substring(1))
+    let str = params.get("id")
+    if (!str) return null
+    let id: number = parseInt(str)
+    if (isNaN(id)) return null
+    return id
+}
+
+let blog_id = ref(get_blog_id())
+if (blog_id.value == null){
+    window.location.href = "/#/Me"//如果id错误就跳转回个人主页
+}
+
 let editor_text = ref("")
 
 function event_post_click(){
