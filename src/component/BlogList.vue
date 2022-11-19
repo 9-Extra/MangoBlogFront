606540cd1afe58b879ce765265e293962f3f07f0
9-Extra <77313011@qq.com>
2022-11-19 14:32:16 +0800

    为个人博客添加进入功能

--- src/component/BlogList.vue
+++ src/component/BlogList.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { ref, reactive, type Ref } from "vue"
 import api from "@/utils/axios_blog";
-import {get_user_information} from "@/utils/user_util";
+import { get_user_information } from "@/utils/user_util";
 import popup_message from "@/utils/message_popup";
 
 interface Blog {
@@ -18,24 +18,24 @@ interface idandaid {
 
 let blog_list: Ref<Blog[]> = ref([])
 
-let postid:idandaid = reactive({
+let postid: idandaid = reactive({
     blog_id: 0,
     operation: "delete"
 })
 
 
-function flash(){
-get_user_information().then(
-    user => {
-        if (user != null) {
-            api.get("/open/blogs/?id=" + user.id).then(response => {
-                blog_list.value = response.data.data
-            }).catch(error => {
-                popup_message("加载失败: " + error.message, "error")
-            })
+function flash() {
+    get_user_information().then(
+        user => {
+            if (user != null) {
+                api.get("/private/blogs").then(response => {
+                    blog_list.value = response.data.data
+                }).catch(error => {
+                    popup_message("加载失败: " + error.message, "error")
+                })
+            }
         }
-    }
-);
+    );
 }
 
 flash();
@@ -46,10 +46,14 @@ function blog_detail(id) {
 
 }
 
-function event_deleteblog(bid){
+function event_open_blog_click(blog_id: number) {
+    window.open("/blog_editor.html?id=" + blog_id)
+}
+
+function event_deleteblog(bid) {
     postid.blog_id = bid;
-    api.post("/post",postid).then(response => {
-        popup_message("删除成功" , "success")
+    api.post("/post", postid).then(response => {
+        popup_message("删除成功", "success")
         flash();
     }).catch(error => {
         popup_message("删除失败: " + error.message, "error")
@@ -67,11 +71,11 @@ function event_deleteblog(bid){
                 <th class="id1">作者id</th>
                 <th class="description">简介</th>
             </tr>
-            <tr v-for=" blog in blog_list">
+            <tr class="blog_line" @click="event_open_blog_click(blog.id)" v-for=" blog in blog_list">
                 <td class="id1">{{ blog.id }}</td>
                 <td class="id1" @click="blog_detail(blog.authorid)">{{ blog.authorid }}</td>
                 <td class="description">{{ blog.description }}
-                <button class="delete" @click="event_deleteblog(blog.id)">删除</button>
+                    <button class="delete" @click.stop="event_deleteblog(blog.id)">删除</button>
                 </td>
             </tr>
         </table>
@@ -79,8 +83,8 @@ function event_deleteblog(bid){
 </template>
 
 <style scoped>
-   .blogbox {
-    
+.blogbox {
+
     justify-content: center;
     align-items: center;
 
@@ -89,47 +93,47 @@ function event_deleteblog(bid){
 
 
 
-.blogbox > ul {
+.blogbox>ul {
 
-margin-top: 20px;
-margin-right: 10px;
-font-size: 40px;
-color: rgba(213, 201, 201, 0.9);
-
-}
-
-span{
-margin-top: 10px;
+    margin-top: 20px;
+    margin-right: 10px;
+    font-size: 40px;
+    color: rgba(213, 201, 201, 0.9);
 
 }
 
-.box > button{
+span {
+    margin-top: 10px;
+
+}
+
+.box>button {
     margin: 10px;
     width: 130px;
     height: 40px;
     border-radius: 20px;
     border: 1px solid rgba(123, 2, 24, 0.5);
     background-color: rgba(123, 2, 24, 0.4);
-    color: rgba(0,0,0,0.7);
+    color: rgba(0, 0, 0, 0.7);
     transition: 1s;
 }
 
 table.imagetable {
-font-family: verdana, arial, sans-serif;
-font-size: 11px;
-color: #333333;
-border-width: 1px;
-border-color: #999999;
-border-collapse: collapse;
+    font-family: verdana, arial, sans-serif;
+    font-size: 11px;
+    color: #333333;
+    border-width: 1px;
+    border-color: #999999;
+    border-collapse: collapse;
 }
 
-table.imagetable th{
-background: rgba(255, 174, 52, 0.724)
+table.imagetable th {
+    background: rgba(255, 174, 52, 0.724)
 }
 
-table.imagetable td{
-background: rgb(255, 245, 106);
-text-align: left;
+table.imagetable td {
+    background: rgb(255, 245, 106);
+    text-align: left;
 
 }
 .id1 {
@@ -173,6 +177,7 @@ text-align: left;
 }
 
 .delete{
+    cursor: pointer;
     float:right;
     margin: 10px;
     width: 5vw;
@@ -188,6 +193,10 @@ text-align: left;
 button:hover {
     border: 1px solid rgba(255, 34, 56, 0.8);
     background-color: rgba(255, 34, 56, 0.838);
+}
+
+.blog_line {
+    cursor: pointer;
 }
 </style>
   
